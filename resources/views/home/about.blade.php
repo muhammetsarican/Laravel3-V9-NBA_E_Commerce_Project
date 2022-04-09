41f2f12038312488b18933e555b5a9b878fb1a50
MuhammetSarican <80002701+MuhammetSarican@users.noreply.github.com>
2022-04-09 04:14:25 +0300

    User işlemleri ve genel taslak oluşturuldu.

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -6,7 +6,7 @@
 @endsection
 @section('content')
     <!-- Title page -->
-    <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('{{asset('assets')}}/images/bg-01.jpg');">
+    <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('{{asset('assets')}}/images/bg-02.jpg');">
         <h2 class="ltext-105 cl0 txt-center">
             About
         </h2>
